2a7880ebe952bdda5b675e1337551179d3104ebe
ClockWize171 <jupiterminhtin998@gmail.com>
2022-09-18 21:57:53 +0630

    reupdate

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -11,6 +11,12 @@ const Navbar = ({ title }) => {
                     <FaGithub className='inline pr-2 text-3xl' />
                     <Link to='/' className='text-lg font-bold align-middle'>{title}</Link>
                 </div>
+                <div className="flex-1 px-2 mx-2">
+                    <div className="flex justify-end">
+                        <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>Home</Link>
+                        <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>About</Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
