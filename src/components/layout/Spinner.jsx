2f1d90167ad33ceac185a8e65bb9f2d21b2e02f9
ClockWize171 <jupiterminhtin998@gmail.com>
2022-09-19 16:26:06 +0630

    added loading.gif and user cards

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import loading from '../layout/assets/loading.gif'
+
+const Spinner = () => {
+  return (
+    <div className='w-100 mt-20'>
+        <img width={180} className='text-center mx-auto' src={loading} alt="Loading...." />
+    </div>
+  )
+}
+
+export default Spinner
